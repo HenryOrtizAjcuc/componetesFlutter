7f87373e1f3413c4cd3af57cf7bdd46f99923654
Henry Ortiz <heoa100@gmail.com>
2021-09-01 12:04:46 -0600

    widget alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,14 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar alerta'),
+          onPressed: () => _mostrarAlerta(context),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blue, shape: StadiumBorder()),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(context);
@@ -14,5 +22,35 @@ class AlertPage extends StatelessWidget {
         child: Icon(Icons.arrow_back),
       ),
     );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Este es el contenido de la caja de alerta'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')),
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Continuar'))
+            ],
+          );
+        });
   }
 }
