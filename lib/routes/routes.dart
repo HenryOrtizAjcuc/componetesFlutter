3ed30c3a26c26d1c44d5c89bfac528abf0f6ef5d
Henry Ortiz <heoa100@gmail.com>
2021-09-01 16:31:05 -0600

    inputFields

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
-    'animated_container': (BuildContext context) => AnimatedContainerPage()
+    'animated_container': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage()
   };
 }
---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: [_crearInput(), Divider(), _crearPersona()],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: false,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+          counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Ingrese su nombre completo',
+          labelText: 'Nombre',
+          helperText: 'Nombre que se mostrara al iniciar sesión',
+          suffix: Icon(
+            Icons.accessibility,
+            color: Colors.blue,
+          ),
+          icon: Icon(Icons.account_circle)),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+    );
+  }
+}
