e3e91d7d67b0a25d8c576033d36794c38fcbbc53
Henry Ortiz <heoa100@gmail.com>
2021-09-01 12:46:52 -0600

    widget circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.minutoneuquen.com/u/fotografias/m/2020/5/25/f850x638-267592_345081_5050.jpg'),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+              child: CircleAvatar(
+                child: Text('SL'),
+                backgroundColor: Colors.brown,
+              ))
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.publinews.gt/gt/wp-content/uploads/2020/11/jennifer-aniston.png'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 600.0,
+        ),
       ),
     );
   }
